ee4b7a3df1b22b789318478d6c1bd41081779994
Mateusz Grzech <grzech.mateusz@gmail.com>
2025-04-09 21:14:05 +0200

    Small changes

---
+++ excel_tables/excel_file.py
@@ -0,0 +1,42 @@
+from abc import ABC
+from typing import Tuple, Optional, List
+
+import openpyxl as xl
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+class ExcelFile(ABC):
+    def __init__(self, file_name: str, worksheet_number: int = 0):
+        self.file_name = file_name
+        self.wb, self.ws = ExcelFile.open_file(file_name, worksheet_number)
+
+    @staticmethod
+    def open_file(file_name: str, worksheet_number: int = 0) -> Tuple[Optional[Workbook], Optional[Worksheet]]:
+        if file_name:
+            try:
+                wb = xl.load_workbook(file_name)
+                ws = wb.worksheets[worksheet_number] if worksheet_number < len(wb.worksheets) else None
+                return wb, ws
+            except Exception as e:
+                print(f"Error opening file {file_name}: {e}")
+                return None, None
+        else:
+            return None, None
+
+    def change_print_area(self, print_area: List[str]) -> None:
+        if self.ws is None:
+            raise ValueError("Worksheet is not initialized")
+            
+        print_area = print_area if print_area else self.ws.print_area
+        self.ws.print_area = print_area
+
+    def save_file(self, new_file_name: str = None) -> None:
+        if self.wb is None:
+            raise ValueError("Workbook is not initialized")
+            
+        new_file_name = new_file_name if new_file_name else self.file_name
+        try:
+            self.wb.save(new_file_name)
+        except Exception as e:
+            print(f"Error saving file {new_file_name}: {e}")
